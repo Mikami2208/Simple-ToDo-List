b218bd1305ae2ccb4ffa9166b7f973284bcdd9a6
Dwightm1 <artemdwightm1@Noutbuk-Dwightm1.local>
2025-10-18 12:55:29 +0300

    add: tailwind setup & base styles

---
+++ src/tailwind.config.ts
@@ -0,0 +1,13 @@
+export default {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    purple: '#6C63FF',
+                    white: '#F7F7F7',
+                    dark: '#252525'
+                }
+            }
+        }
+    }
+}
